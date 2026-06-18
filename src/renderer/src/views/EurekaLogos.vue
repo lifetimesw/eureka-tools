a863bd3abfc3267dcc776914ee3b7b65abf3837a
LifetimeSW <2689076039@qq.com>
2026-06-18 18:12:23 +0800

    update: logos

--- src/renderer/src/views/EurekaLogos.vue
+++ src/renderer/src/views/EurekaLogos.vue
@@ -3,11 +3,11 @@ import { eurekaLogosCrystal, eurekaLogos } from '@renderer/data/eureka.data'
 import { getIcon } from '@renderer/api/request'
 import { Logos } from '@renderer/types/eureka.type'
 
-const logosType = ref(2)
+const logosType = ref(1)
 const logosTypeList = [
-  { type: 1, name: '碎晶' },
-  { type: 2, name: '文理' },
-  { type: 3, name: '文理图鉴' },
+  { type: 1, name: '文理' },
+  { type: 2, name: '文理图鉴' },
+  { type: 3, name: '碎晶' },
 ]
 
 const crystalColumns = [
@@ -25,11 +25,16 @@ const logosColumns = [
   { key: 'description', title: '说明' },
 ]
 
+const checkedLogos = reactive<Record<number, boolean>>({})
+eurekaLogos.forEach((item) => {
+  checkedLogos[item.order] = true
+})
+
 const filterText = ref('')
 
 const tableData = computed(() => {
   const fContent = filterText.value.trim()
-  if (logosType.value === 1) {
+  if (logosType.value === 3) {
     if (fContent) {
       return {
         columns: crystalColumns,
@@ -161,73 +166,100 @@ function getInfoModel(rowData: Logos): string {
       <stone-type v-model="logosType" :list="logosTypeList"></stone-type>
     </div>
     <div class="w-full h-[calc(100%-3em)]">
-      <stone-table v-if="logosType === 1" :table-data="tableData" class="cold" fix-head>
-        <template #name="{ row, value }">
-          <p class="if-center">
-            <img class="icon icon-m" :src="getIcon(row.icon)" />
-            <span class="ml-1">{{ value }}</span>
-          </p>
-        </template>
-        <template #logos="{ value }">
-          <p v-for="(item, index) in value" :key="index" class="f-center-center lh-6">
-            <img class="icon icon-m" :src="getIcon(item.icon)" />
-            <span class="inline-block ml-1 w-6em text-left">{{ item.name }}</span>
-          </p>
-        </template>
-        <template #sources="{ value }">
-          <p v-for="(item, index) in value" :key="index" class="f-center lh-6">
-            <img v-for="icon in item.icons" :key="icon" class="icon icon-m" :src="getIcon(icon)" />
-            <span class="ml-1">{{ item.name }}</span>
-            <span v-if="item.levels.length">：{{ item.levels.join('、') }}</span>
-          </p>
-        </template>
-        <template #description="{ value }">
-          <p class="ws-pre">{{ value }}</p>
-        </template>
-      </stone-table>
-      <stone-table v-else :table-data="tableData" class="cold" fix-head>
-        <template #icon="{ row, value }">
-          <img class="icon icon-xxl" :src="getIcon(value)" v-title:right.template="getInfoModel(row)" />
-        </template>
-        <template #synthesisRecipes="{ value }">
-          <div v-for="(child, index) in value" :key="index" class="f-center">
-            <i class="icon icon-s mr-1 i-lucide:circle-divide"></i>
-            <div v-for="(item, idx) in child" :key="item.icon" class="if-center lh-6">
-              <span v-if="idx !== 0" class="mx-2">+</span>
+      <div class="w-full h-full f-start-center" v-if="logosType === 1">
+        <div class="logos-box">
+          <div
+            v-for="item in eurekaLogos"
+            :key="item.order"
+            class="f-center-center w-full h-full relative"
+            @click="checkedLogos[item.order] = !checkedLogos[item.order]"
+            v-title:bottom.template="getInfoModel(item)">
+            <img class="logos-icon w-full h-full" :src="getIcon('061833')" v-title:bottom.template="getInfoModel(item)" />
+            <img v-if="checkedLogos[item.order]" class="absolute left-1 top-1" :src="getIcon(item.icon)" />
+          </div>
+          <div>
+            <span></span>
+          </div>
+        </div>
+      </div>
+      <div class="w-full h-full" v-else-if="logosType === 2">
+        <stone-table :table-data="tableData" class="cold" fix-head>
+          <template #icon="{ value }">
+            <img class="icon icon-xxl" :src="getIcon(value)" />
+          </template>
+          <template #synthesisRecipes="{ value }">
+            <div v-for="(child, index) in value" :key="index" class="f-center">
+              <i class="icon icon-s mr-1 i-lucide:circle-divide"></i>
+              <div v-for="(item, idx) in child" :key="item.icon" class="if-center lh-6">
+                <span v-if="idx !== 0" class="mx-2">+</span>
+                <img class="icon mr-1" :src="getIcon(item.icon)" />
+                <span>{{ item.name }}</span>
+              </div>
+            </div>
+          </template>
+          <template #jobs="{ value }">
+            <p v-for="item in value" :key="item.icon" class="f-center lh-6">
               <img class="icon mr-1" :src="getIcon(item.icon)" />
               <span>{{ item.name }}</span>
-            </div>
-          </div>
-        </template>
-        <template #jobs="{ value }">
-          <p v-for="item in value" :key="item.icon" class="f-center lh-6">
-            <img class="icon mr-1" :src="getIcon(item.icon)" />
-            <span>{{ item.name }}</span>
-          </p>
-        </template>
-        <template #description="{ value }">
-          <template v-for="(item, index) in value" :key="index">
-            <div v-if="Array.isArray(item)" class="lh-5 text-3 ws-pre-wrap text-left">
-              <template v-for="(sItem, xIdx) in item" :key="sItem">
-                <template v-if="sItem.includes('：')">
-                  <span class="c-green-600" :class="{ 'ml-3': xIdx !== 0 }">{{ sItem.split('：')[0] }}：</span>
-                  <span class="c-#393d44">{{ sItem.split('：')[1] }}</span>
-                </template>
-                <span v-else>{{ sItem }}</span>
-              </template>
-            </div>
-            <p v-else-if="item.includes('：')" class="lh-5 text-3 ws-pre-wrap text-left">
-              <span class="c-green-600">{{ item.split('：')[0] }}：</span>
-              <span class="c-#393d44">{{ item.split('：')[1] }}</span>
-            </p>
-            <p v-else class="lh-5 text-3 ws-pre-wrap text-left">
-              <span>{{ item }}</span>
             </p>
           </template>
-        </template>
-      </stone-table>
+          <template #description="{ value }">
+            <template v-for="(item, index) in value" :key="index">
+              <div v-if="Array.isArray(item)" class="lh-5 text-3 ws-pre-wrap text-left">
+                <template v-for="(sItem, xIdx) in item" :key="sItem">
+                  <template v-if="sItem.includes('：')">
+                    <span class="c-green-600" :class="{ 'ml-3': xIdx !== 0 }">{{ sItem.split('：')[0] }}：</span>
+                    <span class="c-#393d44">{{ sItem.split('：')[1] }}</span>
+                  </template>
+                  <span v-else>{{ sItem }}</span>
+                </template>
+              </div>
+              <p v-else-if="item.includes('：')" class="lh-5 text-3 ws-pre-wrap text-left">
+                <span class="c-green-600">{{ item.split('：')[0] }}：</span>
+                <span class="c-#393d44">{{ item.split('：')[1] }}</span>
+              </p>
+              <p v-else class="lh-5 text-3 ws-pre-wrap text-left">
+                <span>{{ item }}</span>
+              </p>
+            </template>
+          </template>
+        </stone-table>
+      </div>
+      <div class="w-full h-full" v-else-if="logosType === 3">
+        <stone-table :table-data="tableData" class="cold" fix-head>
+          <template #name="{ row, value }">
+            <p class="if-center">
+              <img class="icon icon-m" :src="getIcon(row.icon)" />
+              <span class="ml-1">{{ value }}</span>
+            </p>
+          </template>
+          <template #logos="{ value }">
+            <p v-for="(item, index) in value" :key="index" class="f-center-center lh-6">
+              <img class="icon icon-m" :src="getIcon(item.icon)" />
+              <span class="inline-block ml-1 w-6em text-left">{{ item.name }}</span>
+            </p>
+          </template>
+          <template #sources="{ value }">
+            <p v-for="(item, index) in value" :key="index" class="f-center lh-6">
+              <img v-for="icon in item.icons" :key="icon" class="icon icon-m" :src="getIcon(icon)" />
+              <span class="ml-1">{{ item.name }}</span>
+              <span v-if="item.levels.length">：{{ item.levels.join('、') }}</span>
+            </p>
+          </template>
+          <template #description="{ value }">
+            <p class="ws-pre">{{ value }}</p>
+          </template>
+        </stone-table>
+      </div>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.logos-box {
+  display: grid;
+  grid-template-columns: repeat(10, 3em);
+  grid-template-rows: repeat(6, 3em);
+  gap: 0.25em;
+}
+</style>
