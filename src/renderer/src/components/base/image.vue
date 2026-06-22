95c19a590172230f2a959c4a862532bd1106f186
LifetimeSW <2689076039@qq.com>
2026-06-22 17:46:43 +0800

    update: fix Icons

---
+++ src/renderer/src/components/base/image.vue
@@ -0,0 +1,16 @@
+<template>
+  <img @error="handleError" />
+</template>
+
+<script setup lang="ts">
+import constants from '@renderer/api/constants'
+import { getIcon } from '@renderer/api/request'
+
+function handleError(event: Event): void {
+  const img = event.target as HTMLImageElement
+  img.src = getIcon(constants.unknownIcon)
+  img.onerror = null
+}
+</script>
+
+<style scoped></style>
